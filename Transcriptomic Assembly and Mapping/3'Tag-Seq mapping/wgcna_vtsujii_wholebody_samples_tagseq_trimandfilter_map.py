ad3c6a74cb37130c5416fa5908855212d2f91fcb
Lisa Mesrop <lmesrop@ucsb.edu>
2024-04-10 17:29:06 -0700

    Update wgcna_vtsujii_wholebody_samples_tagseq_trimandfilter_map.py

--- Transcriptomic Assembly and Mapping/3'Tag-Seq mapping/wgcna_vtsujii_wholebody_samples_tagseq_trimandfilter_map.py
+++ Transcriptomic Assembly and Mapping/3'Tag-Seq mapping/wgcna_vtsujii_wholebody_samples_tagseq_trimandfilter_map.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
 #Description: Generate slurm submission scripts to process and map 3'Tag-Seq samples vtu1, vtu2, vtu3, vtu4, vtu5, vtu6, vtu7, vtu8, vtu9, vtu10. 
-#Author: Jessica Goodheart 
+#Author: Jessica Goodheart, Gigi Minsky and Michael S. Drummond
 #Goal: Take the raw fastq files, perform QC checks, filter the adaptor, remove PCR duplicates, trim, and map reads to reference transcriptome. 
+
+
