866aca65de7e05103d8731b5b193553a9d85e04c
Lisa Mesrop <lmesrop@ucsb.edu>
2024-04-10 17:16:51 -0700

    Update and rename vtu1-vtu9_taqseq_trimandfilter_map.py to vtu1-vtu10_taqseq_trimandfilter_map.py

--- Transcriptomic Assembly and Mapping/3'Tag-Seq mapping/vtu1-vtu10_taqseq_trimandfilter_map.py
+++ Transcriptomic Assembly and Mapping/3'Tag-Seq mapping/vtu1-vtu10_taqseq_trimandfilter_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#Description: Generate slurm submission scripts to process and map 3'Tag-Seq samples Vtu1 - Vtu9. 
+#Description: Generate slurm submission scripts to process and map 3'Tag-Seq samples vtu1, vtu2, vtu3, vtu4, vtu5, vtu6, vtu7, vtu8, vtu9, vtu10. 
 #Author: Jessica Goodheart 
 #Goal: Take the raw fastq files, perform QC checks, filter the adaptor, remove PCR duplicates, trim, and map reads to reference transcriptome. 
 
