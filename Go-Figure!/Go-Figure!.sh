694a1c93ef82573bebcd2c16cafdc10779b7e9ee
Lisa Mesrop <lmesrop@ucsb.edu>
2024-04-18 14:50:59 -0700

    Update Go-Figure!.sh

--- Go-Figure!/Go-Figure!.sh
+++ Go-Figure!/Go-Figure!.sh
@@ -22,12 +22,16 @@ Go-Figure! analysis for Vargula tsujii using DE genes (significantly upregulated
 
 python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i GO_figure_DE_Vargulatsujii_df.tsv -o gofigure_directory_DE_Vargula_tsujii_si0.5 -si 0.5 -m 50 -e 100 -q pdf
 
+#new version 
+python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i GO_figure_DE_bio_upper_lip_Vargulatsujii_df_nodes61.tsv -o GO_BioUL_gofigure_directory_si0.5 -e 100 -m 100 -si 0.5 -q pdf
+
+
+
 ### Script 4 
 
 GO-Figure! analysis for all co-expressed genes of the BCN (Vargula tsujii). 
 
 python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i GO_figure_BCN_df.tsv -o gofigure_directory_BCN_si0.1_color -si 0.1 -m 50 -e 100 -q pdf -p Purples
-
 
 
 
