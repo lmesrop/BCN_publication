92705e581873b4da236c626b8c32efe6ba63630d
Lisa Mesrop <lmesrop@ucsb.edu>
2024-05-15 15:25:47 -0700

    Update Go-Figure!.sh

--- Go-Figure!/Go-Figure!.sh
+++ Go-Figure!/Go-Figure!.sh
@@ -7,14 +7,14 @@
 
 Go-Figure! analysis for both species, Vargula tsujii, and Skogsbergia sp. using DE genes (uniquely significantly upregulated) for both luminous and non-luminous upper lips. 
 
-python /home/lmesrop/programs/GO-Figure/gofigure.py -j standard-plus -i subset_Combined_TopGO_GO-Figure_DE_bothspecies.tsv -o gofigure_directory_DE_unique_bothspecies_si0.5 -si 0.5 -c user -m 50 -e 100 -q pdf
+python /home/lmesrop/programs/GO-Figure/gofigure.py -j standard-plus -i subset_Combined_TopGO_GO-Figure_DE_bothspecies.tsv -o gofigure_directory_DE_unique_bothspecies_si0.3 -si 0.3 -c user -m 50 -e 100 -q pdf
 
 
 ### Script 2 
 
 Go-Figure! analysis for Skogsbergia sp. using DE genes (uniquely significantly upregulated) for non-luminous upper lip. 
 
-python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i df_TopGO_Skogsbergia_sp_DE_unique_Upper_Lip_BP.tsv -o gofigure_directory_DE_unique_skogsbergia_sp_si0.5 -si 0.5 -m 25 -e 100 -q pdf
+python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i df_TopGO_Skogsbergia_sp_DE_unique_Upper_Lip_BP.tsv -o gofigure_directory_DE_UL_unique_si0.5 -si 0.5 -m 25 -e 100 -q pdf
 
 
 
