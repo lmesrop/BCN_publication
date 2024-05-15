7760feaa9ec4f0ba681fe6c96971f1b1de367fd9
Lisa Mesrop <lmesrop@ucsb.edu>
2024-05-15 15:05:33 -0700

    Update Go-Figure!.sh

--- Go-Figure!/Go-Figure!.sh
+++ Go-Figure!/Go-Figure!.sh
@@ -5,22 +5,25 @@
 
 ### Script 1
 
-Go-Figure! analysis for both species, Vargula tsujii, and Skogsbergia sp. using DE genes (significantly upregulated) for both luminous and non-luminous upper lips. 
+Go-Figure! analysis for both species, Vargula tsujii, and Skogsbergia sp. using DE genes (uniquely significantly upregulated) for both luminous and non-luminous upper lips. 
 
-python /home/lmesrop/programs/GO-Figure/gofigure.py -j standard-plus -i GO_figure_bothspecies_df_subset.tsv -o gofigure_directory_DE_bothspecies_si0.5 -si 0.5 -c user -m 50 -e 100 -q pdf
+python /home/lmesrop/programs/GO-Figure/gofigure.py -j standard-plus -i subset_Combined_TopGO_GO-Figure_DE_bothspecies.tsv -o gofigure_directory_DE_unique_bothspecies_si0.5 -si 0.5 -c user -m 50 -e 100 -q pdf
+
 
 ### Script 2 
 
-Go-Figure! analysis for Skogsbergia sp. using DE genes (significantly upregulated) for non-luminous upper lip. 
+Go-Figure! analysis for Skogsbergia sp. using DE genes (uniquely significantly upregulated) for non-luminous upper lip. 
 
-python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i GO_figure_DE_Skogsbergia_sp_df.tsv -o gofigure_directory_DE_skogsbergia_sp_si0.5 -si 0.5 -m 25 -e 100 -q pdf
+python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i df_TopGO_Skogsbergia_sp_DE_unique_Upper_Lip_BP.tsv -o gofigure_directory_DE_unique_skogsbergia_sp_si0.5 -si 0.5 -m 25 -e 100 -q pdf
+
 
 
 ### Script 3
 
-Go-Figure! analysis for Vargula tsujii using DE genes (significantly upregulated) for luminous upper lip. 
+Go-Figure! analysis for Vargula tsujii using DE genes (uniquely significantly upregulated) for luminous upper lip. 
 
-python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i GO_figure_DE_bio_upper_lip_Vargulatsujii_df_nodes61.tsv -o GO_BioUL_gofigure_directory_si0.5 -e 100 -m 100 -si 0.5 -q pdf
+python /home/lmesrop/programs/GO-Figure/gofigure.py -j topgo -i df_TopGO_Vargula_tsujii_DE_unique_BioUpperLip_BP.tsv -o gofigure_directory_DE_BioUL_unique_si0.5 -e 100 -m 100 -si 0.5 -q pdf
+
 
 
 ### Script 4 
