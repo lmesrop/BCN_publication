4c905747a3f37d10a76270785694b07993063656
Lisa Mesrop <lmesrop@ucsb.edu>
2024-09-21 15:11:53 -0700

    Update KinFin_downstream_analyses.R

--- KinFin/KinFin_downstream_analyses.R
+++ KinFin/KinFin_downstream_analyses.R
@@ -19,7 +19,7 @@ arthro_only_proteins_removep <-  arthro_only_proteins %>% separate(V1, c( "trans
 lum_only_proteins_removep <- lum_only_proteins %>% separate(V1, c( "transcript_id", "col2"), ".p") %>% select(-c(col2))
 lux_only_proteins_removep <- lux_only_proteins %>% separate(V1, c( "transcript_id", "col2"), ".p") %>% select(-c(col2))
 
-## import dataframes for significantly upregulated genes of the luminous upper lip, compound eye and gut
+## import dataframes for significantly upregulated (unique) of the luminous upper lip, compound eye and gut
 
 #luminous upper lip 
 df_bio_upper_lip
@@ -32,11 +32,11 @@ df_gut
 #BCN
 red_module_gene_annot_power_8_prefilter_5_3$transcript_id
 
-##extract just the names and rename for clarity 
+##extract the transcript ids and rename for clarity 
 #BCN
 BCN_module <- subset(red_module_gene_annot_power_8_prefilter_5_3, select = "transcript_id") 
 
-## create a dataframe for the total number of transcripts that have predicted ORFs (from the OrthoFinder and KinFin analyses).Take the txt files from the KinFin output - arthro_proteins.txt and Vargula_tsujii_cdhit_95_proteins.txt - and combine them into a single dataframe ALL_arthro_and_vtsujii_specific_proteins and remove nested transcripts that have been counted twice and removed .p* 
+##create a dataframe for the total number of transcripts that have predicted ORFs (from the OrthoFinder and KinFin analyses). Take the txt files from the KinFin output and combine them into a single dataframe. 
 ALL_arthro_and_vtsujii_specific_proteins_rm <- ALL_arthro_and_vtsujii_specific_proteins %>% distinct()
 ALL_arthro_and_vtsujii_specific_proteins_rm_removep <- ALL_arthro_and_vtsujii_specific_proteins_rm %>% separate(...1, c( "transcript_id", "col2"), ".p") %>% select(-c(col2))
 #rename for clarity 
@@ -48,7 +48,7 @@ ALL_transcripts_with_predictedORFs
 #all expressed DGE transcripts (prefilter min 2 counts in more than 5 biological replicates or more). Imported from Juypter Notebook. 
 ALL_DGE_min2counts <- subset(dds_count_table_organ_level_prefiltered_strict_assay_ids, select = "transript_ids")
 
-#### function for finding taxonomic restricted genes for each dataset of interest (e.g. significantly upregulated genes and co-expressed genes)
+#### function for finding taxonomically restricted genes for each dataset of interest (e.g. significantly upregulated genes and co-expressed genes)
 
 ## first, combine the dataframes from the KinFin output for each taxonomic level (w/o nested proteins)
 
@@ -59,7 +59,7 @@ names_list_1 <- c("arthro_only_proteins_removep", "ostra_only_proteins_removep",
 ## rename the lists to the corresponding taxonomic level 
 names(list1) <- names_list_1
 
-#function to input the dataset of interest and determine how many are restricted to each taxonomic level of interest and print out the number of transcripts for each taxonmic level 
+#function to input the dataset of interest and determine how many are restricted to each taxonomic level of interest and print out the number of transcripts for each taxonomic level 
 
 match_transcripts_to_taxonomic_origin <- function(input_df, match_list) {
   match_chars <- function(row, df) {
