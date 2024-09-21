9467106a6f1eb7ed071a2f42cb02372edc985c19
Lisa Mesrop <lmesrop@ucsb.edu>
2024-09-21 15:25:18 -0700

    Update Skogsbergia_sp_transcriptome.sh

--- Transcriptomic Assembly and Mapping/Skogsbergia_sp_transcriptome.sh
+++ Transcriptomic Assembly and Mapping/Skogsbergia_sp_transcriptome.sh
@@ -10,5 +10,5 @@ cd-hit-est -i /home/lmesrop/skogs_belize_transcriptome/trinity_out_dir/Trinity.f
 #run TransDecoder 
 
 TransDecoder.LongOrfs -t /home/lmesrop/skogs_belize_transcriptome/skogs_cdhit_90_longest_isoform_fasta_transdecoder_bowtie2_050922/skogs_cdhit_90_longestisoform.fasta 
-TransDecoder.Predict -t /home/lmesrop/skogs_belize_transcriptome/skogs_cdhit_90_longest_isoform_fasta_transdecoder_bowtie2_050922/skogs_cdhit_90_longestisoform.fasta
+TransDecoder.Predict -t /home/lmesrop/skogs_belize_transcriptome/skogs_cdhit_90_longest_isoform_fasta_transdecoder_bowtie2_050922/skogs_cdhit_90_longestisoform.fasta -single_best_orf
 
